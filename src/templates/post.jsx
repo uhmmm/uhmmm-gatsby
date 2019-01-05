6d57dbaa904724a6719963043a9b92f89d4d8938
Zaturrby <rjkorteschiel@gmail.com>
2019-01-05 17:29:10 +0100

    define fonts

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import Layout from "../layout";
-import UserInfo from "../components/UserInfo/UserInfo";
-import Disqus from "../components/Disqus/Disqus";
-import PostTags from "../components/PostTags/PostTags";
-import SocialLinks from "../components/SocialLinks/SocialLinks";
-import SEO from "../components/SEO/SEO";
-import config from "../../data/SiteConfig";
-import "./b16-tomorrow-dark.css";
-import "./post.css";
+import React from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+import Layout from '../layout'
+import UserInfo from '../components/UserInfo/UserInfo'
+import Disqus from '../components/Disqus/Disqus'
+import PostTags from '../components/PostTags/PostTags'
+import SocialLinks from '../components/SocialLinks/SocialLinks'
+import SEO from '../components/SEO/SEO'
+import config from '../../data/SiteConfig'
+import './b16-tomorrow-dark.css'
+import './post.css'
 
 export default class PostTemplate extends React.Component {
   render() {
-    const { slug } = this.props.pageContext;
-    const postNode = this.props.data.markdownRemark;
-    const post = postNode.frontmatter;
+    const { slug } = this.props.pageContext
+    const postNode = this.props.data.markdownRemark
+    const post = postNode.frontmatter
     if (!post.id) {
-      post.id = slug;
+      post.id = slug
     }
     if (!post.category_id) {
-      post.category_id = config.postDefaultCategoryID;
+      post.category_id = config.postDefaultCategoryID
     }
     return (
       <Layout>
@@ -34,14 +34,14 @@ export default class PostTemplate extends React.Component {
             <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
             <div className="post-meta">
               <PostTags tags={post.tags} />
-              <SocialLinks postPath={slug} postNode={postNode} />
+              {/* <SocialLinks postPath={slug} postNode={postNode} /> */}
             </div>
             <UserInfo config={config} />
-            <Disqus postNode={postNode} />
+            {/* <Disqus postNode={postNode} /> */}
           </div>
         </div>
       </Layout>
-    );
+    )
   }
 }
 
@@ -69,4 +69,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
