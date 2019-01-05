9c13131057f8c7a2c33f149a22698d470d57fa1a
Zaturrby <rjkorteschiel@gmail.com>
2019-01-05 19:33:38 +0100

    first elements

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,11 +6,9 @@ import styles from './index.module.css'
 
 export default class MainLayout extends React.Component {
   render() {
-    console.log(styles)
     const { children } = this.props
     return (
       <div className={styles.container}>
-        asdjhkfasldkjhfalksjhdfalskjdhf
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,17 +6,46 @@ import Layout from '../layout'
 import PostListing from '../components/PostListing/PostListing'
 import SEO from '../components/SEO/SEO'
 import config from '../../data/SiteConfig'
+import bgback from './bg-back.png'
+import bgfront from './bg-front.png'
+import title from './title.png'
 
 class Index extends React.Component {
   render() {
-    console.log(styles)
     const postEdges = this.props.data.allMarkdownRemark.edges
     return (
       <Layout>
         <div className={styles.container}>
           <Helmet title={config.siteTitle} />
-          {/* <SEO /> */}
-          <PostListing postEdges={postEdges} />
+          <div className={styles.grid}>
+            {/* <SEO /> */}
+            <div className={styles.display}>
+              <img className={styles.title} src={title} />
+              <img className={styles.bgback} src={bgback} />
+              <img className={styles.bgfront} src={bgfront} />
+            </div>
+            <div className={styles.firstParagraph}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic,
+                cumque debitis rerum quod, excepturi velit consequuntur in ex
+                aperiam temporibus adipisci animi libero dolorum sunt eveniet
+                vitae suscipit, commodi sapiente!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic,
+                cumque debitis rerum quod, excepturi velit consequuntur in ex
+                aperiam temporibus adipisci animi libero dolorum sunt eveniet
+                vitae suscipit, commodi sapiente!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic,
+                cumque debitis rerum quod, excepturi velit consequuntur in ex
+                aperiam temporibus adipisci animi libero dolorum sunt eveniet
+                vitae suscipit, commodi sapiente!
+              </p>
+            </div>
+            <PostListing postEdges={postEdges} />
+          </div>
         </div>
       </Layout>
     )
