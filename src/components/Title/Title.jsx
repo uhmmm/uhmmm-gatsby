e098d9038ce8ced0db148c09e721d9a3c6f3ba38
Zaturrby <rjkorteschiel@gmail.com>
2019-01-06 21:36:22 +0100

    component knockouttext works

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -4,13 +4,11 @@ import title from './title2x.png'
 
 import styles from './Title.module.css'
 
-export default ({ name }) => {
-  console.log(name)
-  return (
-    <div className={styles.outer}>
-      <div className={styles.container}>
-        <img className={styles.title} src={title} />
-      </div>
+export default ({ name }) => (
+  <div className={styles.outer}>
+    <div className={styles.container}>
+      <div className={styles.title}>{name}</div>
+      {/* <img className={styles.title} src={title} /> */}
     </div>
-  )
-}
+  </div>
+)
