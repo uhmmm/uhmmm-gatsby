2c70d096b599e84361141c883145c02bcb34f881
Zaturrby <rjkorteschiel@gmail.com>
2019-01-06 20:04:36 +0100

    factor stuff out to layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+
+import Gradient from '../components/Gradient/Gradient'
+import Bar from '../components/Bar/Bar'
+
 import config from '../../data/SiteConfig'
+
 import './global.css'
 import styles from './index.module.css'
 
@@ -12,7 +17,11 @@ export default class MainLayout extends React.Component {
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
+        {/* DO NOT PLACE ANYTHING VISISBLE BEFORE THE GRADIENT */}
+        <Gradient />
+        <Bar type="horizontal" />
         {children}
+        <Bar type="horizontal" />
       </div>
     )
   }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import cx from 'classnames'
 import { graphql } from 'gatsby'
-import styles from './index.module.css'
+
 import Layout from '../layout'
 import PostListing from '../components/PostListing/PostListing'
-import Gradient from '../components/Gradient/Gradient'
 import Showcase from '../components/Showcase/Showcase'
 import Contact from '../components/Contact/Contact'
 import Navbar from '../components/Navbar/Navbar'
-import Bar from '../components/Bar/Bar'
 import SEO from '../components/SEO/SEO'
+
 import config from '../../data/SiteConfig'
+
+import styles from './index.module.css'
 
 class Index extends React.Component {
   render() {
@@ -19,14 +19,11 @@ class Index extends React.Component {
     return (
       <Layout>
         <Helmet title={config.siteTitle} />
-        {/* <SEO /> */}
-        <Gradient />
-        <Bar type="horizontal" />
+        <SEO />
         <Navbar />
         <Showcase />
         <PostListing postEdges={postEdges} />
         <Contact />
-        <Bar type="horizontal" />
       </Layout>
     )
   }
