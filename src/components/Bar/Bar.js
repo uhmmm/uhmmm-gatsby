d682a0411e214fd6c69814c0605c95e7f6fce6de
Zaturrby <rjkorteschiel@gmail.com>
2019-08-02 12:15:52 +0200

    all elements visible again, z-index issue due to absolute position of grid

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -5,6 +5,8 @@ import styles from './Bar.module.css'
 
 export default ({ type }) => (
   <div className={styles.container}>
-    <img className={styles.bgtop} src={bgtop} />
+    <div className={styles.imageContainer}>
+      <img className={styles.imageDark} src={bgtop} />
+    </div>
   </div>
 )
