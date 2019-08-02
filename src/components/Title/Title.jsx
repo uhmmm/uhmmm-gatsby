770bb805547020e8a37ee0cb8572c7ade4f5a470
Zaturrby <rjkorteschiel@gmail.com>
2019-08-02 18:25:12 +0200

    another step

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,9 +2,4 @@ import React from 'react'
 
 import styles from './Title.module.css'
 
-export default ({ name }) => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>{name}</h1>
-    {/* <img className={styles.title} src={title} /> */}
-  </div>
-)
+export default ({ name }) => <h1 className={styles.title}>{name}</h1>
