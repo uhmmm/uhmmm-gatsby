39fee4c0223ec844d669da07453e17d498dfb70e
Zaturrby <rjkorteschiel@gmail.com>
2019-08-05 19:26:44 +0200

    me like emotion

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,22 +5,23 @@ import PersonList from '../PersonList/PersonList'
 
 import styles from './Contact.module.css'
 
-// const subtitle =
-
 const Subtitle = styled.h2(({ theme: { type, grid } }) => [
   type.templates.subtitle,
   { padding: `0 0 ${grid.unit} 0` }
 ])
+
+const Subtext = styled.p(({ theme: { type } }) => [type.templates.subtext])
 
 const Contact = () => {
   return (
     <div className={styles.container} id="contact">
       <div className={styles.contact}>
         <Subtitle>contact</Subtitle>
-        <p className={styles.text}>
+        <Subtext>
           Uhmmm is based in Amsterdam and New York. Our headquarters are in
           Amsterdam.
-        </p>
+        </Subtext>
+
         <ul>
           <li className={styles.text}>Uhmmm.net</li>
           <li className={styles.text}>Orteliuskade 4G</li>
@@ -32,7 +33,7 @@ const Contact = () => {
         </ul>
       </div>
       <div className={styles.people}>
-        <h2 className={styles.title}>members</h2>
+        <Subtitle>members</Subtitle>
         <PersonList />
       </div>
     </div>
