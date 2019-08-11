d1fba109d1d90295fc60dd614c445c2609d8d3b0
Zaturrby <rjkorteschiel@gmail.com>
2019-08-11 12:01:36 +0200

    chip / tag

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import cx from 'classnames'
+import styled from '@emotion/styled'
+import { math } from 'polished'
 
-import styles from './Chip.module.css'
+import { colors, type, grid } from '../Styles'
+
+const Container = styled.div(({ backgroundColor }) => ({
+  display: 'inline-block',
+  padding: math(`${grid.unit} / 2`),
+  margin: `0 ${grid.unit} ${grid.unit} 0`,
+
+  background: colors[backgroundColor] || 'unset',
+
+  ...type.caption,
+  color: colors.day
+}))
 
 export default ({ text, color }) => (
-  <div className={cx(styles.container, styles[color])}>
-    <span>{text}</span>
-  </div>
+  <Container backgroundColor={color}>{text}</Container>
 )
